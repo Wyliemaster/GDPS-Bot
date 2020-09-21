1be96e56bbad12420b93ffd64d4d705ca5cff1ed
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2020-09-21 12:00:20 +0100

    added the bot's github in credits command

--- commands/credits.js
+++ commands/credits.js
@@ -5,7 +5,7 @@ const botconfig = require("../botsettings.json");
 module.exports.run = async (bot, message, args) => {
 const credits = new Discord.MessageEmbed()
 .setTitle('GDPS Bot by Wylie')
-.setURL('https://github.com/Wyliemaster')
+.setURL('https://github.com/Wyliemaster/GDPS-Bot')
 .addField('Credits', '[GDColon](https://github.com/GDColon) for giving me tips to write this and for letting me use a bit of GDBrowsers parsing algorithm \n[RealstikDash](https://github.com/RealistikDash) for helping me with user inputs and server stuff')
 message.channel.send(credits)
 }
