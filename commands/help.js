d5268674ba4b2bfb8d5582e92cb6f5eeb382672b
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-01-01 16:09:23 +0000

    ok fixed lul
    im an idiot, its because i forgot to put the token in lmfao

--- commands/help.js
+++ commands/help.js
@@ -3,10 +3,10 @@ const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, message, args) => {
     const help = new Discord.MessageEmbed()
-    .setTitle('__Help and Commands__')
-    .addField('__Help__', 'if you need support please contact [wylie](https://twitter.com/TheWylieMaster). he can be contacted [here](https://discord.gg/r5KW2bR)')
-    .addField('__Commands__', '``' + botconfig.prefix + 'profile {Player name or Account ID}``\n\n``' + botconfig.prefix + 'level {Level name or Level ID}``')
-    .setColor('0x' + botconfig.commandColour)
+        .setTitle('__Help and Commands__')
+        .addField('__Help__', 'if you need support please contact [wylie](https://twitter.com/TheWylieMaster). he can be contacted [here](https://discord.gg/r5KW2bR)')
+        .addField('__Commands__', '`' + botconfig.prefix + 'profile {Player name or Account ID}`\n\n`' + botconfig.prefix + 'level {Level name or Level ID}`\n\n`' + botconfig.prefix + 'leaderboard {star/cp}`')
+        .setColor('0x' + botconfig.commandColour)
     message.channel.send(help)
 }
 
--- commands/settings.js
+++ commands/settings.js
@@ -3,14 +3,14 @@ const botconfig = require("../botsettings.json");
 
 
 module.exports.run = async (bot, message, args) => {
-message.channel.send('**__Here are the settings__** \nserver: ``' + botconfig.server + '``\nPrefix: ``' + botconfig.prefix+ '``')
+    message.channel.send('**__Here are the settings__** \nserver: `' + botconfig.server + '`\nPrefix: `' + botconfig.prefix + '`')
 }
 
 
 module.exports.config = {
     name: "settings",
     description: "Fetches the profile data for the accounts you search",
-    usage: botconfig.prefix +"settings",
+    usage: botconfig.prefix + "settings",
     accessableby: "Members",
     aliases: ['s']
 }
