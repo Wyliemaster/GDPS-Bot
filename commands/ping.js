bafce323b3d9a69b078f02ef61d66f7c619f45bb
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-02-11 16:06:04 +0000

    request delay for ping
    added extra info to ping to check how long it takes for the server to respond

--- commands/ping.js
+++ commands/ping.js
@@ -3,17 +3,19 @@ const botconfig = require("../botsettings.json");
 const request = require('request');
 
 module.exports.run = async (bot, message, args) => {
-request.get('http://' + botconfig.server + '/incl/lib/connection.php', function callback(httpResponse, err, body){
-    if(err['statusCode'] == 404){
-        message.reply(err['statusCode'] + ' ' + err['statusMessage'] + '\nCannot find Connection.php. Check console for more details')
-        console.log("if you get this message, it means that you haven't set up the config correctly, please add your server to the config")
-}   else if(err['statusCode'] != 200){
-    message.reply( err['statusCode'] + ' ' + err['statusMessage'] + '\nServers are down')
-}
-    else {
-        message.reply(err['statusCode'] + ' ' + err['statusMessage'] + '\nServers are fine!')
-    }
-} )
+    let commandStart = Math.floor(Date.now())
+    request.get('http://' + botconfig.server + '/incl/lib/connection.php', function callback(httpResponse, err, body) {
+        if (err['statusCode'] == 404) {
+            message.reply(err['statusCode'] + ' ' + err['statusMessage'] + '\nCannot find Connection.php. Check console for more details')
+            console.log("if you get this message, it means that you haven't set up the config correctly, please add your server to the config")
+        } else if (err['statusCode'] != 200) {
+            message.reply(err['statusCode'] + ' ' + err['statusMessage'] + '\nServers are down')
+        }
+        else {
+            let commandEnd = Math.floor(Date.now())
+            message.reply(err['statusCode'] + ' ' + err['statusMessage'] + '\nServers are fine!\nServer Speed: ' + (commandEnd - commandStart).toString() + 'ms')
+        }
+    })
 }
 
 
